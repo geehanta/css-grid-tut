ebf8ad9e6a600f1a8e98dbdc3e33769955b3855b
geehunter <gh@localhost.localdomain>
2022-02-23 16:32:56 -0500

    tweaked content

--- gridpage.php
+++ gridpage.php
@@ -18,6 +18,35 @@
             <article class="cardContainer">
                 <div class="card1" id="card1shadow">   
                     <div id="box1"></div>
+                    <div class="box2"></div>
+                    <div class="box3">
+                    <form class="border" method="get" action="loginSchool.php">
+        <br>
+        <br>
+        <label for ="name">Name</label>
+        <input type="text" name="name"> <br><br>
+        <label for ="adm">Reg No</label>
+        <input type="text" name="adm"> <br><br>
+        <label for ="password">Password</label>
+        <input type="text" name="pwd"> <br>
+        <br>
+        
+        <button class="btn" type="submit">Sign In</button>
+        <br>
+        <br>
+        <p style="padding: 10px;">You don't have an account? <a href="inputSchool.php">Register</a></p>
+    </form>
+    <?php 
+        $s1name =$_GET["name"];
+        $s1adm =$_GET["adm"];
+        $s1pwd =$_GET["pwd"];
+       
+    
+        echo "You are $s1name <br>";
+        echo "of admission number $s1adm <br>";
+        echo "Remember to change this immediately $s1pwd <br><br>";
+        ?>
+                    </div>
                         </div>
             </article>
             <footer>
