8f3cf8ece60d256d296e2c17b994a1553f3e7d2e
geehunter <gh@localhost.localdomain>
2022-02-23 16:00:50 -0500

    started grid page

---
+++ gridpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE Html>
+<html>
+    <head>
+        <title>Flexbox</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="gridstyles.css">
+    </head>
+    <body>
+        <main>
+            <header>
+                <nav>
+                    <ul>
+                        <li><link a href="aboutUs.php">About Us</a>  </li>
+                    </ul>
+                </nav>
+
+            </header>
+            <article class="cardContainer">
+                <div class="card1" id="card1shadow">   
+                    <div id="box1"></div>
+                        </div>
+            </article>
+            <footer>
+
+            </footer>
+        </main>
+    </body>
+</html>
